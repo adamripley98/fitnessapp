f9cff65ac590cd606ba8752a6b20a3c28dd479c5
Jeffrey Chen <jeffreychen@Jeffreys-MacBook-Pro.local>
2017-08-01 15:40:06 -0700

    fix paths, add map

--- reactNativeApp/App.js
+++ reactNativeApp/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './Login';
+import Login from './components/Login';
 
 export default class App extends React.Component {
     render() {
         return (
           <View style={styles.container}>
-            <Login />
+            <Text>hi Joe</Text>
           </View>
         );
     }
